3526171a23caed726c07e4e1ce2e66dac5d4b379
Mauricio Reyes <mreyes@talpor.com>
2017-09-22 18:59:02 -0400

    fix compilation problem

--- contracts/LotusPreSale.sol
+++ contracts/LotusPreSale.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.11;
 import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 import './LotusToken.sol';
 
 contract LotusPreSale is Crowdsale {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
-var LotusToken = artifacts.require("./LotusToken.sol");
-var LotusPreSale = artifacts.require("./LotusPreSale.sol");
+var LotusToken = artifacts.require('./LotusToken.sol');
+var LotusPreSale = artifacts.require('./LotusPreSale.sol');
 
-var PRESALE_PRICE = 12000 * (10 ** 8);
+var PRICE = 12000 * (10 ** 8);
+var START_TIME = new Date(2017, 1, 1, 0, 0, 0, 0).getTime();
+var END_TIME = new Date(2017, 11, 1, 0, 0, 0, 0).getTime();
+var WALLET = '0x93e66d9baea28c17d9fc393b53e3fbdd76899dae';
 
 module.exports = function(deployer) {
   deployer.deploy(LotusToken);
   deployer.link(LotusToken, LotusPreSale);
-  deployer.deploy(LotusPreSale, 1, 999, PRESALE_PRICE,
-                  0x93e66d9baea28c17d9fc393b53e3fbdd76899dae);
+  deployer.deploy(LotusPreSale, START_TIME, END_TIME, PRICE, WALLET);
 };
