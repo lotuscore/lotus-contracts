ebe0bb7652f5c6ae217450165120f284096cdf9d
Mauricio Reyes <mreyes@talpor.com>
2017-09-23 11:57:47 -0400

    minor changes in deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,10 @@
-var LotusToken = artifacts.require('./LotusToken.sol');
-var LotusPreSale = artifacts.require('./LotusPreSale.sol');
+const LotusPreSale = artifacts.require('./LotusPreSale.sol')
 
-var PRICE = 12000 * (10 ** 8);
-var START_TIME = new Date(2017, 1, 1, 0, 0, 0, 0).getTime();
-var END_TIME = new Date(2017, 11, 1, 0, 0, 0, 0).getTime();
-var WALLET = '0x93e66d9baea28c17d9fc393b53e3fbdd76899dae';
+const RATE = 12000 * (10 ** 8)
+const START_TIME = new Date(2017, 1, 1, 0, 0, 0, 0).getTime()
+const END_TIME = new Date(2017, 11, 1, 0, 0, 0, 0).getTime();
+const WALLET = '0x93e66d9baea28c17d9fc393b53e3fbdd76899dae'
 
 module.exports = function(deployer) {
-  deployer.deploy(LotusToken);
-  deployer.link(LotusToken, LotusPreSale);
-  deployer.deploy(LotusPreSale, START_TIME, END_TIME, PRICE, WALLET);
+  deployer.deploy(LotusPreSale, START_TIME, END_TIME, RATE, WALLET)
 };
