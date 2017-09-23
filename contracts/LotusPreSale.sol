a5d0e2e0d0ff555c3576a52994ac7398d4c43378
Mauricio Reyes <mreyes@talpor.com>
2017-09-23 10:02:19 -0400

    fix compilation problem

--- contracts/LotusPreSale.sol
+++ contracts/LotusPreSale.sol
@@ -1,10 +1,16 @@
 pragma solidity ^0.4.11;
+
 import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
-import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 import './LotusToken.sol';
 
 contract LotusPreSale is Crowdsale {
+
+  function LotusPreSale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet)
+    Crowdsale(_startTime, _endTime, _rate, _wallet) {
+  }
+
   function createTokenContract() internal returns (MintableToken) {
     return new LotusToken();
   }
+
 }
