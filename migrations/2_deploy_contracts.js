04fb5d355053afa752d7fa7a3f00a7512d4838d7
Mauricio Reyes <mreyes@talpor.com>
2017-09-26 10:58:27 -0400

    use enviroment variables to deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,13 @@
 const LotusPreSale = artifacts.require('./LotusPreSale.sol')
+const LotusToken = artifacts.require('./LotusToken.sol')
 
-const RATE = 12000 * (10 ** 8)
-const START_TIME = new Date(2017, 1, 1, 0, 0, 0, 0).getTime()
-const END_TIME = new Date(2017, 11, 1, 0, 0, 0, 0).getTime();
-const WALLET = '0x93e66d9baea28c17d9fc393b53e3fbdd76899dae'
+const RATE = 12000 * (10 ** 18)
+const START_TIME = process.env.PS_START_TIME
+const END_TIME = process.env.PS_END_TIME
+const WALLET = process.env.PS_WALLET
 
 module.exports = function(deployer) {
+  deployer.deploy(LotusToken);
+  deployer.link(LotusToken, LotusPreSale);
   deployer.deploy(LotusPreSale, START_TIME, END_TIME, RATE, WALLET)
 };
