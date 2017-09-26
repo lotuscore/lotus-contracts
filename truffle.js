a8a189f7fa29410d89184e14c8523a550516e5e1
Mauricio Reyes <mreyes@talpor.com>
2017-09-26 12:50:49 -0400

    add rinkeby provider

--- truffle.js
+++ truffle.js
@@ -1,9 +1,16 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(process.env.MNEMONIC,
+        "https://rinkeby.infura.io/"+process.env.INFURA_KEY),
+      network_id: 3
     }
   }
 };
