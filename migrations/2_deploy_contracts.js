7957f0b236fa9c0d79d8759ecb904304cfd5078e
Mauricio Reyes <mreyes@talpor.com>
2017-10-04 14:53:10 -0400

    use human readable environment variables

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
+/* globals  artifacts */
 const LotusPreSale = artifacts.require('./LotusPreSale.sol')
-const LotusToken = artifacts.require('./LotusToken.sol')
 
-const RATE = 12000 * (10 ** 18)
-const START_TIME = process.env.PS_START_TIME
-const END_TIME = process.env.PS_END_TIME
+const RATE = 12000 // 12000 tokens per ether
+const START_TIME = Math.round(
+  new Date(...process.env.PS_START_TIME.split('-')).getTime() / 1000)
+const END_TIME = Math.round(
+  new Date(...process.env.PS_END_TIME.split('-')).getTime() / 1000)
 const WALLET = process.env.PS_WALLET
 
 module.exports = function(deployer) {
-  deployer.deploy(LotusToken);
-  deployer.link(LotusToken, LotusPreSale);
   deployer.deploy(LotusPreSale, START_TIME, END_TIME, RATE, WALLET)
 };
