7f335ace3430a4611f97b438395abc2a52125bd4
Mauricio Reyes <mreyes@talpor.com>
2017-10-05 17:38:17 -0400

    transfer token ownership at the end of the presale

---
+++ contracts/LotusPresale.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+import './LotusToken.sol';
+
+contract LotusPresale is CappedCrowdsale {
+
+  function LotusPresale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, address _wallet)
+    CappedCrowdsale(_cap) Crowdsale(_startTime, _endTime, _rate, _wallet) {
+  }
+
+  function createTokenContract() internal returns (MintableToken) {
+    return new LotusToken();
+  }
+
+  /**
+   * @dev In order to allow minting in the public crowdsale, transfer control
+   * of the token contract to the funding wallet
+   */
+  function transferTokenOwnership() public {
+    require(hasEnded());
+    token.transferOwnership(wallet);
+  }
+
+}
