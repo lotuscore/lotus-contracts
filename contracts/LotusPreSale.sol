ae4fb3d24c91347aa29f0ebd17ea27e7ed1c1782
Mauricio Reyes <mreyes@talpor.com>
2017-10-04 20:43:17 -0400

    cap the presale

--- contracts/LotusPreSale.sol
+++ contracts/LotusPreSale.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.11;
 
-import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 import './LotusToken.sol';
 
-contract LotusPreSale is Crowdsale {
+contract LotusPreSale is CappedCrowdsale {
 
-  function LotusPreSale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet)
-    Crowdsale(_startTime, _endTime, _rate, _wallet) {
+  function LotusPreSale(uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, address _wallet)
+    CappedCrowdsale(_cap) Crowdsale(_startTime, _endTime, _rate, _wallet) {
   }
 
   function createTokenContract() internal returns (MintableToken) {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@
 const LotusPreSale = artifacts.require('./LotusPreSale.sol')
 
 const RATE = 12000 // 12000 tokens per ether
+const CAP = 8334 * (10 ** 18)
 const START_TIME = Math.round(
   new Date(...process.env.PS_START_TIME.split('-')).getTime() / 1000)
 const END_TIME = Math.round(
@@ -9,5 +10,5 @@ const END_TIME = Math.round(
 const WALLET = process.env.PS_WALLET
 
 module.exports = function(deployer) {
-  deployer.deploy(LotusPreSale, START_TIME, END_TIME, RATE, WALLET)
+  deployer.deploy(LotusPreSale, START_TIME, END_TIME, RATE, CAP, WALLET)
 };
