83aae50f61287625a2f8dbe21ca3ed4653eb4411
Mauricio Reyes <mreyes@talpor.com>
2017-11-03 17:38:39 -0400

    change naming pattern

--- migrations/2_deploy_presale.js
+++ migrations/2_deploy_presale.js
@@ -8,15 +8,15 @@ const CAP = 8334 * (10 ** 18)
 const RELEASE_DATE = Math.round(
   new Date(...process.env.RELEASE_DATE.split('-')).getTime() / 1000)
 const START_TIME = Math.round(
-  new Date(...process.env.PS_START_TIME.split('-')).getTime() / 1000)
+  new Date(...process.env.PRESALE_START.split('-')).getTime() / 1000)
 const END_TIME = Math.round(
-  new Date(...process.env.PS_END_TIME.split('-')).getTime() / 1000)
+  new Date(...process.env.PRESALE_END.split('-')).getTime() / 1000)
 const RESERVE_ACCOUNT = process.env.RESERVE_ACCOUNT
 const FUND_ACCOUNT = process.env.FUND_ACCOUNT
 
 module.exports = function(deployer) {
-  assert(END_TIME > START_TIME)
-  assert(RELEASE_DATE > END_TIME)
+  assert(END_TIME > START_TIME, 'END_TIME must be greater than START_TIME')
+  assert(RELEASE_DATE > END_TIME, 'RELEASE_DATE must be greater than END_TIME')
   deployer.deploy(LotusToken, RESERVE_ACCOUNT, RELEASE_DATE)
   deployer.deploy(LotusPresale, START_TIME, END_TIME, RATE, CAP, FUND_ACCOUNT)
   deployer.then(async function () {
