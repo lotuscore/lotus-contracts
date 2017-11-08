47f938875a1bcdd6ab1e844c597a12a1c5a58512
Mauricio Reyes <mreyes@talpor.com>
2017-11-08 14:40:05 -0400

    use dotenv

--- migrations/2_deploy_presale.js
+++ migrations/2_deploy_presale.js
@@ -1,4 +1,5 @@
 /* globals  artifacts */
+require('dotenv').config({ path: '../.env' })
 const assert = require('assert')
 const LotusPresale = artifacts.require('./LotusPresale.sol')
 const LotusToken = artifacts.require('./LotusToken.sol')
@@ -15,6 +16,8 @@ const RESERVE_ACCOUNT = process.env.RESERVE_ACCOUNT
 const FUND_ACCOUNT = process.env.FUND_ACCOUNT
 
 module.exports = function(deployer) {
+  const now = Math.round(new Date().getTime() / 1000)
+  assert(START_TIME > now, 'START_TIME must be in the future')
   assert(END_TIME > START_TIME, 'END_TIME must be greater than START_TIME')
   assert(RELEASE_DATE > END_TIME, 'RELEASE_DATE must be greater than END_TIME')
   deployer.deploy(LotusToken, RESERVE_ACCOUNT, RELEASE_DATE)
